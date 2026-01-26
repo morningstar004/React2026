b4c4328644bcb860f27a33e82e7f9d4f6fa94414
morningstar004 <pranjalkumar000007@gmail.com>
2026-01-26 23:04:38 +0530

    learned about the use of useState Hook in array

--- 09_useStateInArrayAndObject/src/main.jsx
+++ 09_useStateInArrayAndObject/src/main.jsx
@@ -2,7 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Form from './neatedForm.jsx'
+import TaskApp from './UseStateOnArray.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <Form />
+    <TaskApp />
 )
