4fd9c6ad778e6f45918698dbde82c3d65d170ac0
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-01 08:19:18 +0530

    Fixed the API issue with the Currency convert and set the Backedcode of converter and customHook use visual changes still need to be done

--- 11_Currency_convertor/src/hooks/useCurrencyInfo.js
+++ 11_Currency_convertor/src/hooks/useCurrencyInfo.js
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react'
-
+import { useEffect, useState } from "react";
 
 const useCurrencyInfo = (currency) => {
-    const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-
-    },[currency])
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`,
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]))
+      .catch((err) => console.error("Error fetching currency data:", err));
+  }, [currency]);
 
   return data;
-}
+};
 
-export default useCurrencyInfo
+export default useCurrencyInfo;
