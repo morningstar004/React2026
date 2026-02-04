db74bd76249396ee4dc17c48444ab83035c8eaf4
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-04 22:41:01 +0530

    added UI to the 14 project the ThemeSwitcher

--- 14_theme_switcher/src/App.jsx
+++ 14_theme_switcher/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Card from "./component/Card";
+import ThemeBtn from "./component/ThemeBtn";
 
 function App() {
-
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-gray-900 transition-colors duration-500">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-4">Theme Switcher</h1>
-        <p className="text-lg text-gray-700 dark:text-gray-300">This is a simple theme switcher using Tailwind CSS and React.</p>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
