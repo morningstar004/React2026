a43c13f71fd8bb0e81ad76250a9661b6eb90db83
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-05 23:57:13 +0530

    Added the components of the project

--- 15_todo_context/src/App.jsx
+++ 15_todo_context/src/App.jsx
@@ -1,20 +1,29 @@
 import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoCard from "./components/TodoCard";
+import { TodoProvider } from './contexts/todoContext.js'
+import { useState } from "react";
+
 
 function App() {
+
+  
   return (
-    <>
+    <TodoProvider value={{addTodo,deleteTodo,editTodo,toggleTodo,Todos}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4"><TodoForm /></div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+            {Todos.map((todo) => (<div key={todo.id} className="w-full">
+              <TodoCard todo={todo} />
+            </div>))}
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   );
 }
 
--- 15_todo_context/src/main.jsx
+++ 15_todo_context/src/main.jsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { TodoProvider } from './contexts/todoContext.js'
 
 createRoot(document.getElementById('root')).render(
-  <TodoProvider>
+  
     <App />
-  </TodoProvider>
+  
 )
