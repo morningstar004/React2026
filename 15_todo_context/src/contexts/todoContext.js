cc2438f575580493b6b6da69c37fbf2f5b528d62
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-05 22:42:22 +0530

    added fundamental context to the project

---
+++ 15_todo_context/src/contexts/todoContext.js
@@ -0,0 +1,21 @@
+import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+    Todos: [
+        {
+            id: 1,
+            title: "react learn",
+            completed: false
+        }
+    ],
+    addTodo: (title) => {},
+    editTodo: (title,id) => {},
+    deleteTodo: (id) => {},
+    toggleTodo: (id) => {}
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider
--- 15_todo_context/src/main.jsx
+++ 15_todo_context/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { TodoProvider } from './contexts/todoContext.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <TodoProvider>
     <App />
-  </StrictMode>,
+  </TodoProvider>
 )
