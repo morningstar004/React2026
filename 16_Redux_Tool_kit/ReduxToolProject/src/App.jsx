1cb124c34ba82b25e02a7375fb4e944d0371a2e0
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-09 23:11:00 +0530

    app.jsx built of project 16

--- 16_Redux_Tool_kit/ReduxToolProject/src/App.jsx
+++ 16_Redux_Tool_kit/ReduxToolProject/src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-
+  
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-center mt-10'>
-        Hello world!
-      </h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
