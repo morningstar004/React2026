f79363626f322c8e34f9f93c2b8906a47ddc07c4
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-15 00:13:57 +0530

    added services for file uplode

--- 17_Funtioning_site/src/appWrite/config.js
+++ 17_Funtioning_site/src/appWrite/config.js
@@ -92,6 +92,38 @@ export class Service {
   }
 
   //file upload services
+  async uplodeFile(file){
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketID,
+        ID.unique(),
+        file,
+      )
+    } catch (error) {
+      console.log("Appwrite service :: uploadeFile :: error ", error);
+      return false
+    }
+  }
+
+  async deleteFile(fileId){
+    try {
+      await this.bucket.deleteFile(
+        conf.appwriteBucketID,
+        fileId
+      )
+      return true
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile :: error ", error);
+      return false
+    }
+  }
+
+  getFilePreview(fileId){
+    return this.bucket.getFilePreview(
+      conf.appwriteBucketID,
+      fileId
+    )
+  }
 }
 
 const service = new Service();
