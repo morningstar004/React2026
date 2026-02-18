3ea3167697df82a3c9064840dde286296ce31cc1
morningstar004 <pranjalkumar000007@gmail.com>
2026-02-18 21:59:33 +0530

    logoutBtn component constructed and added Header component features

---
+++ 17_Funtioning_site/src/components/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {useDispatch} from "react-redux";
+import authServise from '../appWrite/auth';
+import { Logout } from '../store/authSlice';
+
+const LogoutBtn = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    authServise.logout.then(() => {
+      dispatch(logout());
+    })
+  }
+  return (
+    <div className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+      logout
+    </div>
+  )
+}
+
+export default LogoutBtn
--- 17_Funtioning_site/src/components/index.js
+++ 17_Funtioning_site/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Logo from "./logo";
+import Container from "./Container";
+import LogoutBtn from "./LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer ,Logo ,Container ,LogoutBtn };
