0398b3260df0508821aecd066a96542a5bce066c
Pranjal Kumar <pranjalkumar000007@gmail.com>
2026-03-06 12:40:59 +0530

    Update auth.js

--- 17_Funtioning_site/src/appWrite/auth.js
+++ 17_Funtioning_site/src/appWrite/auth.js
@@ -14,12 +14,12 @@ export class AuthServise {
 
   async createAccount({ email, password, name }) {
     try {
-      const userAccount = await this.account.create({
-        userID: ID.unique(),
+      const userAccount = await this.account.create(
+        ID.unique(),
         email,
         password,
         name
-      });
+      );
       if (userAccount) {
         //call another method
       } else {
