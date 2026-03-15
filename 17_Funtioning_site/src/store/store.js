be008d3c2a8b98ae779651193930635df1a32c4a
Pranjal Kumar <pranjalkumar000007@gmail.com>
2026-03-15 23:41:11 +0530

    Update store.js

--- 17_Funtioning_site/src/store/store.js
+++ 17_Funtioning_site/src/store/store.js
@@ -8,3 +8,5 @@ const store = configureStore({
 });
 
 export default store;
+
+//React Router DOM is a library for handling navigation in React applications, not a data storage solution itself. Data that you might be tempted to store in React state often has a more natural home in dedicated state management solutions like Redux, the Context API, or even the URL search parameters. 
